545228470e0b70051914b8d3ecec235c0a785fea
Anapat Jiamwijitkul <pat_sk135@hotmail.com>
2020-09-20 17:17:01 +0700

    build(docker): init docker

---
+++ backend/src/app.ts
@@ -0,0 +1,16 @@
+import express, { Request, Response } from 'express'
+import cors from 'cors'
+
+const main = async () => {
+    const app = express()
+    app.use(cors())
+    app.get('/', (req: Request, res: Response) => {
+        res.status(200).send('Hello World!')
+    })
+
+    app.listen(8000, () => {
+        console.log('Server Started at Port, 8000')
+    })
+}
+
+main()
